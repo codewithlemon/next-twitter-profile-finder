69cc8d37a26dcdde0eda53b41320e74596f1f93a
codewithlemon <https://twitter.com/codewithlemon>
2022-10-02 19:55:45 +0800

    components/UserProfile.jsx: support saving profiles

--- components/UserProfile.jsx
+++ components/UserProfile.jsx
@@ -1,7 +1,23 @@
+import { useContext } from "react"
+import { SavedProfilesContext } from "../context/SavedProfiles"
+
 const UserProfile = ({ user }) => {
+  const { savedProfiles, addProfile } = useContext(SavedProfilesContext)
+
+  const profileIsSaved = savedProfiles.some(
+    (savedProfile) => savedProfile.id === user.id
+  )
+
+  function handleClick() {
+    addProfile({
+      username: user.username,
+      id: user.id,
+    })
+  }
+
   return (
-    <div className="max-w-[24rem] mx-auto pt-3">
-      <div className="grid grid-cols-[auto_1fr]">
+    <div className="max-w-[26rem] mx-auto pt-3 grid grid-cols-[1fr_auto]">
+      <div className="grid grid-cols-[auto_1fr] mb-1">
         <p className="text-right pr-2 font-medium">Twitter ID:</p>
         <p>{user.id}</p>
         <p className="text-right pr-2 font-medium">Username:</p>
@@ -17,6 +33,17 @@ const UserProfile = ({ user }) => {
         </p>
         <p className="text-right pr-2 font-medium">Display name:</p>
         <p>{user.name}</p>
+      </div>
+      <div className="text-center">
+        {!profileIsSaved && (
+          <button
+            type="button"
+            className="h-10 px-4 py-2 sm:ml-2 rounded-lg bg-gray-100 hover:bg-gray-200 transition duration-200 text-gray-700 font-normal"
+            onClick={handleClick}
+          >
+            Save
+          </button>
+        )}
       </div>
     </div>
   )
